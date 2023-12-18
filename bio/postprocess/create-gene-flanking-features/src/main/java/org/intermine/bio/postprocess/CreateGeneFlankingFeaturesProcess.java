7b5829aa740b08abf3310b6380bcfe9e3c2a0c9f
Sam Hokin <shokin@ncgr.org>
2023-12-18 14:35:05 -0700

    Added supercontig processing. Some code housekeeping.

--- bio/postprocess/create-gene-flanking-features/src/main/java/org/intermine/bio/postprocess/CreateGeneFlankingFeaturesProcess.java
+++ bio/postprocess/create-gene-flanking-features/src/main/java/org/intermine/bio/postprocess/CreateGeneFlankingFeaturesProcess.java
@@ -14,26 +14,36 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
+
 import org.intermine.bio.util.BioQueries;
 import org.intermine.bio.util.PostProcessUtil;
+
 import org.intermine.metadata.ClassDescriptor;
+import org.intermine.metadata.ConstraintOp;
 import org.intermine.metadata.MetaDataException;
+
 import org.intermine.model.bio.Chromosome;
 import org.intermine.model.bio.DataSet;
 import org.intermine.model.bio.DataSource;
 import org.intermine.model.bio.Gene;
 import org.intermine.model.bio.GeneFlankingRegion;
 import org.intermine.model.bio.Location;
+import org.intermine.model.bio.Supercontig;
+
 import org.intermine.objectstore.ObjectStore;
 import org.intermine.objectstore.ObjectStoreException;
 import org.intermine.objectstore.ObjectStoreWriter;
+import org.intermine.objectstore.query.ContainsConstraint;
 import org.intermine.objectstore.query.Query;
 import org.intermine.objectstore.query.QueryClass;
+import org.intermine.objectstore.query.QueryCollectionReference;
 import org.intermine.objectstore.query.Results;
 import org.intermine.objectstore.query.ResultsRow;
 
@@ -49,13 +59,12 @@ import org.intermine.postprocess.PostProcessor;
  * @author rns
  * @author Sam Hokin
  */
-public class CreateGeneFlankingFeaturesProcess extends PostProcessor
-{
+public class CreateGeneFlankingFeaturesProcess extends PostProcessor {
+    private static final Logger LOG = Logger.getLogger(CreateGeneFlankingFeaturesProcess.class);
 
     private ObjectStore os;
     private DataSet dataSet;
     private DataSource dataSource;
-    private Map<Integer, Chromosome> chrs = new HashMap<Integer, Chromosome>();
 
     // The sizes in kb of flanking regions to create.
     private static double[] distances = new double[] {0.5, 1, 2, 5, 10};
@@ -65,8 +74,6 @@ public class CreateGeneFlankingFeaturesProcess extends PostProcessor
 
     // SH: don't include genes at all
     private static boolean[] includeGenes = new boolean[] {false};
-
-    private static final Logger LOG = Logger.getLogger(CreateGeneFlankingFeaturesProcess.class);
 
     /**
      * Create a new instance
@@ -95,135 +102,109 @@ public class CreateGeneFlankingFeaturesProcess extends PostProcessor
 
     /**
      * {@inheritDoc}
-     * <br/>
-     * Main post-processing routine.
-     *
-     * @throws ObjectStoreException if the objectstore throws an exception
      */
     public void postProcess() throws ObjectStoreException {
-        Results results = BioQueries.findLocationAndObjects(os,
-                Chromosome.class, Gene.class, false, false, false, 1000);
-
-        dataSet = (DataSet) DynamicUtil.createObject(Collections
-                .singleton(DataSet.class));
+        // DataSet - store later
+        dataSet = (DataSet) DynamicUtil.createObject(Collections.singleton(DataSet.class));
         dataSet.setName("InterMine gene-flanking regions");
         dataSet.setDescription("Gene-flanking regions created by the core InterMine post-processor");
         dataSet.setVersion("" + new Date()); // current time and date
         dataSet.setUrl("http://www.intermine.org");
         dataSet.setDataSource(dataSource);
 
-        deleteFlankingRegions();
+        // query Genes that lack flanking regions
+        Query q = new Query();
+        QueryClass qcGene = new QueryClass(Gene.class);
+        q.addFrom(qcGene);
+        q.addToSelect(qcGene);
+        QueryClass qcGeneFlankingRegion = new QueryClass(GeneFlankingRegion.class);
+        q.addFrom(qcGeneFlankingRegion);
+        QueryCollectionReference flankingRegionsRef = new QueryCollectionReference(qcGene, "flankingRegions");
+        q.setConstraint(new ContainsConstraint(flankingRegionsRef, ConstraintOp.IS_NULL));
 
-        Iterator<?> resIter = results.iterator();
         int count = 0;
-        osw.beginTransaction();
-        while (resIter.hasNext()) {
-            ResultsRow<?> rr = (ResultsRow<?>) resIter.next();
-            Integer chrId = (Integer) rr.get(0);
-            Gene gene = (Gene) rr.get(1);
-            Location loc = (Location) rr.get(2);
-            createAndStoreFlankingRegion(getChromosome(chrId), loc, gene);
-            if ((count % 1000) == 0) {
-                LOG.info("Created flanking regions for " + count + " genes.");
+        Results results = os.execute(q, 1000, true, true, true);
+        if (results.asList().size() > 0) {
+            osw.beginTransaction();
+            for (Object obj : results.asList()) {
+                ResultsRow rr = (ResultsRow) obj;
+                Gene gene = (Gene) rr.get(0);
+                createAndStoreFlankingRegions(gene);
+                count++;
             }
-            count++;
+            osw.store(dataSet);
+            osw.commitTransaction();
+            LOG.info("Stored flanking regions for " + count + " genes.");
+        } else {
+            LOG.info("All genes have flanking regions.");
         }
-        osw.store(dataSet);
-        osw.commitTransaction();
     }
 
     /**
-     * Delete existing flanking regions so that we do not store duplicates with a second run.
+     * Create and store the various flanking regions for a given Gene.
+     *
+     * @param gene the Gene
      */
-    private void deleteFlankingRegions() throws ObjectStoreException {
-        Query qGeneFlankingRegion = new Query();
-        QueryClass qcGeneFlankingRegion = new QueryClass(GeneFlankingRegion.class);
-        qGeneFlankingRegion.addFrom(qcGeneFlankingRegion);
-        qGeneFlankingRegion.addToSelect(qcGeneFlankingRegion);
-        List<GeneFlankingRegion> gfrList = new ArrayList<>();
-        Results gfrResults = osw.getObjectStore().execute(qGeneFlankingRegion);
-        for (Object o : gfrResults.asList()) {
-            ResultsRow row = (ResultsRow) o;
-            GeneFlankingRegion gfr = (GeneFlankingRegion) row.get(0);
-            gfrList.add(gfr);
-        }
-        for (GeneFlankingRegion gfr : gfrList) {
-            osw.beginTransaction();
-            osw.delete(gfr);
-            osw.commitTransaction();
-        }
-        System.out.println("### Deleted " + gfrList.size() + " GeneFlankingRegion objects.");
-        LOG.info("### Deleted " + gfrList.size() + " GeneFlankingRegion objects.");
-    }
-    
-    private void createAndStoreFlankingRegion(Chromosome chr, Location geneLoc, Gene gene)
-            throws ObjectStoreException {
-        // This code can't cope with chromosomes that don't have a length
-        if (chr.getLength() == null) {
-            LOG.warn("Attempted to create GeneFlankingRegions on a chromosome without a length: "
-                    + chr.getPrimaryIdentifier());
-            return;
+    private void createAndStoreFlankingRegions(Gene gene) throws ObjectStoreException {
+        boolean onChromosome = (gene.getChromosome() != null);
+        boolean onSupercontig = (gene.getSupercontig() != null);
+        if (!onChromosome && !onSupercontig) {
+            throw new RuntimeException("Gene " + gene.getId() + " is not on a chromosome or a supercontig.");
         }
 
-        // If there is Gene.source attribute we are in modMine, only create flanking regions for
-        // genes from FlyBase and WormBase, not those generated by submissions.
-        ClassDescriptor cld = this.os.getModel().getClassDescriptorByName("Gene");
-        if (cld.getAttributeDescriptorByName("source") != null) {
-            String source;
-            try {
-                source = (String) gene.getFieldValue("source");
-            } catch (IllegalAccessException e) {
-                // This shouldn't happen
-                return;
-            }
-            if (!("FlyBase".equals(source) || "WormBase".equals(source))) {
-                return;
-            }
-
-            // HACK if modMine don't include genes in regions
-            includeGenes = new boolean[] {false};
+        Chromosome chromosome = null;
+        Supercontig supercontig = null;
+        Location geneLoc = null;
+        if (onChromosome) {
+            chromosome = gene.getChromosome();
+            geneLoc = gene.getChromosomeLocation();
+        } else {
+            supercontig = gene.getSupercontig();
+            geneLoc = gene.getSupercontigLocation();
         }
 
+        // run through the various flanking region distances and both directions
         for (double distance : distances) {
             for (String direction : directions) {
                 for (boolean includeGene : includeGenes) {
                     String strand = geneLoc.getStrand();
-
-                    // TODO what do we do if strand not set?
                     int geneStart = geneLoc.getStart().intValue();
                     int geneEnd = geneLoc.getEnd().intValue();
-                    int chrLength = chr.getLength().intValue();
-
-                    // gene touches a chromosome end so there isn't a flanking region
-                    if ((geneStart <= 1) || (geneEnd >= chrLength)) {
+                    int contigLength = 0;
+                    if (onChromosome) {
+                        contigLength = chromosome.getLength().intValue();
+                    } else {
+                        contigLength = supercontig.getLength().intValue();
+                    }
+                    // gene touches a contig end so there isn't a flanking region
+                    if ((geneStart <= 1) || (geneEnd >= contigLength)) {
                         continue;
                     }
-
-                    GeneFlankingRegion region = (GeneFlankingRegion) DynamicUtil
-                            .createObject(Collections.singleton(GeneFlankingRegion.class));
-                    Location location = (Location) DynamicUtil
-                            .createObject(Collections.singleton(Location.class));
-
-                    region.setDistance("" + distance + "kb");
+                    // create this GeneFlankingRegion
+                    GeneFlankingRegion region = (GeneFlankingRegion) DynamicUtil.createObject(Collections.singleton(GeneFlankingRegion.class));
+                    Location location = (Location) DynamicUtil.createObject(Collections.singleton(Location.class));
+                    region.setDistance(distance + "kb");
                     region.setDirection(direction);
                     try {
-                        PostProcessUtil.checkFieldExists(os.getModel(), "GeneFlankingRegion",
-                                "includeGene", "Not setting");
+                        PostProcessUtil.checkFieldExists(os.getModel(), "GeneFlankingRegion", "includeGene", "Not setting");
                         region.setFieldValue("includeGene", Boolean.valueOf(includeGene));
                     } catch (MetaDataException e) {
                         // GeneFlankingRegion.includeGene not in model so do nothing
                     }
                     region.setGene(gene);
-                    region.setChromosome(chr);
-                    region.setChromosomeLocation(location);
+                    if (onChromosome) {
+                        region.setChromosome(chromosome);
+                        region.setChromosomeLocation(location);
+                    } else {
+                        region.setSupercontig(supercontig);
+                        region.setSupercontigLocation(location);
+                    }
                     region.setOrganism(gene.getOrganism());
 		    region.setStrain(gene.getStrain());
-                    region.setPrimaryIdentifier(gene.getPrimaryIdentifier() + " " + distance + "kb "
-                            + direction);
+                    region.setPrimaryIdentifier(gene.getPrimaryIdentifier() + "_" + distance + "_kb_" + direction);
 
                     // this should be some clever algorithm
                     int start, end;
-
                     if ("upstream".equals(direction) && "1".equals(strand)) {
                         start = geneStart - (int) Math.round(distance * 1000);
                         end = includeGene ? geneEnd : geneStart - 1;
@@ -241,15 +222,18 @@ public class CreateGeneFlankingFeaturesProcess extends PostProcessor
                     // if the region hangs off the start or end of a chromosome set it to finish
                     // at the end of the chromosome
                     location.setStart(Math.max(start, 1));
-                    int e = Math.min(end, chr.getLength().intValue());
+                    int e = Math.min(end, contigLength);
                     location.setEnd(e);
-
                     location.setStrand(strand);
-                    location.setLocatedOn(chr);
+                    if (onChromosome) {
+                        location.setLocatedOn(chromosome);
+                    } else {
+                        location.setLocatedOn(supercontig);
+                    }
                     location.setFeature(region);
-
                     region.setLength((location.getEnd().intValue() - location.getStart().intValue()) + 1);
 
+                    // store
                     osw.store(location);
                     osw.store(region);
                 }
@@ -257,12 +241,4 @@ public class CreateGeneFlankingFeaturesProcess extends PostProcessor
         }
     }
 
-    private Chromosome getChromosome(Integer chrId) throws ObjectStoreException {
-        Chromosome chr = chrs.get(chrId);
-        if (chr == null) {
-            chr = (Chromosome) os.getObjectById(chrId, Chromosome.class);
-            chrs.put(chrId, chr);
-        }
-        return chr;
-    }
 }
